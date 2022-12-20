e98d0ad8bdaf0e1d54d258514d279e27693f1d2e
Daniel León Paulin <dleon29@alumnos.uaq.mx>
2022-12-19 23:53:27 -0600

    Update - Shreck, Spiderman & Rick

--- Assets/src/Scripts/CambiarModelo.cs
+++ Assets/src/Scripts/CambiarModelo.cs
@@ -6,9 +6,9 @@ public class CambiarModelo : MonoBehaviour
 {
     public GameObject planeta;
     public GameObject casa;
-    // public GameObject rick;
-    // public GameObject shrek;
-    // public GameObject spiderman;
+    public GameObject rick;
+    public GameObject shrek;
+    public GameObject spiderman;
 
     float contadorCambiarModelo = 0; 
     int modeloMostrado = 1;
@@ -30,43 +30,43 @@ public class CambiarModelo : MonoBehaviour
                 case 1:
                     planeta.SetActive(true);
                     casa.SetActive(false);
-                    // rick.SetActive(false);
-                    // shrek.SetActive(false);
-                    // spiderman.SetActive(false);
+                    rick.SetActive(false);
+                    shrek.SetActive(false);
+                    spiderman.SetActive(false);
                     modeloMostrado = 2;
                     break;
                 case 2:
                     planeta.SetActive(false);
                     casa.SetActive(true);
-                    // rick.SetActive(false);
-                    // shrek.SetActive(false);
-                    // spiderman.SetActive(false);
+                    rick.SetActive(false);
+                    shrek.SetActive(false);
+                    spiderman.SetActive(false);
+                    modeloMostrado = 3;
+                    break;
+                case 3:
+                    planeta.SetActive(false);
+                    casa.SetActive(false);
+                    rick.SetActive(true);
+                    shrek.SetActive(false);
+                    spiderman.SetActive(false);
+                    modeloMostrado = 4;
+                    break;
+                case 4:
+                    planeta.SetActive(false);
+                    casa.SetActive(false);
+                    rick.SetActive(false);
+                    shrek.SetActive(true);
+                    spiderman.SetActive(false);
+                    modeloMostrado = 5;
+                    break;
+                case 5:
+                    planeta.SetActive(false);
+                    casa.SetActive(false);
+                    rick.SetActive(false);
+                    shrek.SetActive(false);
+                    spiderman.SetActive(true);
                     modeloMostrado = 1;
                     break;
-                // case 3:
-                //     planeta.SetActive(false);
-                //     casa.SetActive(false);
-                //     rick.SetActive(true);
-                //     shrek.SetActive(false);
-                //     spiderman.SetActive(false);
-                //     modeloMostrado = 4;
-                //     break;
-                // case 4:
-                //     planeta.SetActive(false);
-                //     casa.SetActive(false);
-                //     rick.SetActive(false);
-                //     shrek.SetActive(true);
-                //     spiderman.SetActive(false);
-                //     modeloMostrado = 5;
-                //     break;
-                // case 5:
-                //     planeta.SetActive(false);
-                //     casa.SetActive(false);
-                //     rick.SetActive(false);
-                //     shrek.SetActive(false);
-                //     spiderman.SetActive(true);
-                //     modeloMostrado = 1;
-                //     break;
             }
             contadorCambiarModelo = 0;
         }
